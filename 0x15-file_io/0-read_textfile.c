cf734c778a41433527dd833325054b179d6ac319
ezzkessem8 <92451799+ezzkessem8@users.noreply.github.com>
2023-08-11 00:34:01 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,53 +1,21 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
 #include "main.h"
+#include <stdlib.h>
 
-/**
- * read_textfile - reads and prints from a file
- * @filename: path to file
- * @letters: chars to read
- * Return: chars read
- */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	char *buff;
-	ssize_t bytes, r;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	if (!filename)
-		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
-		close(fd);
 		return (0);
-	}
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	buff = malloc(sizeof(char) * letters);
-	if (!buff)
-	{
-		close(fd);
-		return (0);
-	}
-
-	bytes = read(fd, buff, letters);
-
-	if (bytes == -1)
-	{
-		close(fd);
-		free(buff);
-		return (0);
-	}
-
-	r = write(STDOUT_FILENO, buff, bytes);
-
-	if (r == -1)
-	{
-		close(fd);
-		free(buff);
-		return (0);
-	}
+	free(buf);
 	close(fd);
-	return (bytes);
+	return (w);
 }
